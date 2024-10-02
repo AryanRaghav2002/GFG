4bcca2acd40be9fdec927f945206a6da06d017a0
Aryan <aryan9911335242@gmail.com>
2024-10-03 00:14:35 +0530

    .

--- Array/Rotate_and_delete.java
+++ Array/Rotate_and_delete.java
@@ -1,10 +1,29 @@
 class Solution {
+    public static void reverse(ArrayList<Integer> arr, int start, int end){
+        while(start<=end){
+            int temp=arr.get(end);
+            arr.set(end, arr.get(start));
+            arr.set(start, temp);
+            start++;
+            end--;
+        }
+    }
+    public static void rotate(ArrayList<Integer> arr){
+        reverse(arr, 0, arr.size()-2);
+        reverse(arr, 0, arr.size()-1);
+    }
+    
+    
     public static int rotateDelete(ArrayList<Integer> arr) {
         // code here
         int n=arr.size();
-        int temp=n/2;
-        int i= 3*(temp/2);
-        if(temp%2==1) i++;
-        return arr.get(n-i);
+        int k=1;
+        while(n>k){
+            rotate(arr);
+            arr.remove(n-k);
+            k++;
+            n--;
+        }
+        return arr.get(0);
     }
 }
